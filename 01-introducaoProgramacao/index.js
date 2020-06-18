46553e4d07f0ee0956e6b7afc895ddd152dc1ebb
vininribeiro23 <vininribeiro@hotmail.com>
2020-06-18 00:33:53 -0300

    primeiro comit

---
+++ 01-introducaoProgramacao/index.js
@@ -0,0 +1,18 @@
+const aluno01 = 'Irailton'
+const notaAluno01 = 9.8
+
+const aluno02 = 'Renato'
+const notaAluno02 = 10
+
+const aluno03 = 'vinin'
+const notaAluno3 = 2
+
+const media = (notaAluno01 + notaAluno02 + notaAluno3) / 3
+
+
+if (media > 5) {
+console.log(`A media foi de ${media}. Parabéns`)
+}else{
+    console.log(`Amedia é menor que 5`)
+
+}
