44ab2cf04cd6092eeb6adad81d9e8c6f94ea1bf1
vininribeiro23 <vininribeiro@hotmail.com>
2020-06-18 20:17:28 -0300

    Operadores

---
+++ operadores.js
@@ -0,0 +1,64 @@
+// Operadores Relacionais
+
+// > maior que
+// < menor que
+//>= maior igual a
+// <= menor igual  a
+
+//const a = 5
+//const b = 6
+
+//console.log(a > b) //false
+//console.log( a < b)//true
+//console.log( a >= b)//false
+//console.log( a <= b)//true
+
+
+//operadores de Igualdade
+
+// == igual a
+// != diferente de
+// === igual e do mesmo tipo
+// !== diferente inclusive  do tipo 
+
+
+//operadores de valor
+
+//const a = 5
+//const b = '5'
+
+//console.log(a == b)
+//console.log(a === b)
+//console.log(a != b)
+//console.log(a !== b)
+
+//Operadores Logico
+
+//&&, ||, !=, !
+//const a = 5
+//const b = '5'
+
+//console.log(a == b && a !== b )//  true
+//console.log(a == b && a != b )// fallse
+
+//console.log(a == b || a !== b )// true
+//console.log(a == b || a != b )//true
+//console.log(a != b || a === b )//true
+
+//console.log( !(a != b || a === b ))//false
+
+
+
+//operadores Aritméticos
+
+// * Multiplicação
+// / Divisão
+// % Resto da Divisão
+// + adição
+// - subtração
+
+//console.log( 2 * 2)    //4
+//console.log (2 / 2)    //1
+//console.log(2 % 1.4)  // 0.6
+//console.log(2 + 2)     //4
+//console.log(2 - 2)     //0
