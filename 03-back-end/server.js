2ff145fa32218c87fd16f65d7a80cb8505828c5b
vininribeiro23 <vininribeiro@hotmail.com>
2020-08-17 22:53:44 -0300

    usando templete engine

--- 03-back-end/server.js
+++ 03-back-end/server.js
@@ -1,9 +1,23 @@
 const express = require ('express')
+const nunjucks = require('nunjucks')
 const server = express()
 
-server.get("/", function(req, res) {
-    return res.send("HI!")
+server.use(express.static('public'))
+
+server.set('view engine', "html")
+
+nunjucks.configure('views', {
+    express: server
 })
+server.get("/", function(req, res) {
+    return res.render("about")
+})
+
+server.get("/portfolio", function(req, res) {
+    return res.render("portfolio")
+})
+
+
 
 server.listen(5000, function(){
     console.log("server is running")
