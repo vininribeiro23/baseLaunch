bcb5348dc9f0976fee7e0b04dba5072585cf7f14
vininribeiro23 <vininribeiro@hotmail.com>
2020-08-20 21:30:34 -0300

    colocando dados no servidor

--- 03-back-end/server.js
+++ 03-back-end/server.js
@@ -9,10 +9,22 @@ server.use(express.static('public'))
 server.set('view engine', "njk")
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 server.get("/", function(req, res) {
-    return res.render("about")
+    const  about = {
+        avatar_url:"https://avatars3.githubusercontent.com/u/48262427?s=460&u=ad9ac289551d12e41b95d12dd659c68458a2b913&v=4",
+        name: "vinicius Ribeiro",
+        role: "Desenvolvedor",
+        description: "Programador Full-Stack, focado nas tecnologias mais atuais do mercado.",
+        links: [
+        { name: "Github", url:"https://github.com/vininribeiro23/"},
+        { name: "Linkedin", url:"https://br.linkedin.com/in/vinicius-ribeiro-409761186/"},
+        ]
+    }
+
+    return res.render("about", { about })
 })
 
 server.get("/portfolio", function(req, res) {
